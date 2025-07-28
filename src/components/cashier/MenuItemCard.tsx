8aad035f6ce3eb1d5a87a61c8aa48b49a830d88d
Rifo Sulthan <rifosulthan2052@gmail.com>
2025-07-28 20:34:06 +0000

    i think for the menu on menuitemcard , I want the placeholder to be the

--- src/components/cashier/MenuItemCard.tsx
+++ src/components/cashier/MenuItemCard.tsx
@@ -21,10 +21,6 @@ function getHintFromItemName(name: string): string {
 }
 
 function getInitials(name: string): string {
-  const words = name.split(' ');
-  if (words.length > 1) {
-    return (words[0][0] + (words[1]?.[0] || '')).toUpperCase();
-  }
   return name.substring(0, 2).toUpperCase();
 }
 

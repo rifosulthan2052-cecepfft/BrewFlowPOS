070852974fc1f08eaada474f3634fc16d69d1152
Rifo Sulthan <rifosulthan2052@gmail.com>
2025-07-28 17:18:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/cashier/MenuItemCard.tsx
+++ src/components/cashier/MenuItemCard.tsx
@@ -23,7 +23,7 @@ function getHintFromItemName(name: string): string {
 function getInitials(name: string): string {
   const words = name.split(' ');
   if (words.length > 1) {
-    return (words[0][0] + words[1][0]).toUpperCase();
+    return (words[0][0] + (words[1]?.[0] || '')).toUpperCase();
   }
   return name.substring(0, 2).toUpperCase();
 }

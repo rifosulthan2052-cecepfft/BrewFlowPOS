24458c4f527dd280cb18a9f798718c1a23bc15eb
Rifo Sulthan <rifosulthan2052@gmail.com>
2025-07-29 21:57:49 +0000

    yeah.. still not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,9 +7,9 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDI0zK7GwIY3N1cIy191Sfq-RWYAkxjg6E",
-  authDomain: "brew-flow.firebaseapp.com",
+  authDomain: "6000-firebase-studio-1753700681526.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev",
   projectId: "brew-flow",
-  storageBucket: "brew-flow.firebasestorage.app",
+  storageBucket: "brew-flow.appspot.com",
   messagingSenderId: "436443450780",
   appId: "1:436443450780:web:a1bd5f8ef317a5229d0c59",
 };

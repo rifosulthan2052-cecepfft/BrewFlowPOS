911c49fc75f5d8c98ffed7a000e4794a5b9cd558
Rifo Sulthan <rifosulthan2052@gmail.com>
2025-08-12 10:42:38 +0000

    one last check, is this app PWA ready?
    or are we still long way from th

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#6F4E37" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
